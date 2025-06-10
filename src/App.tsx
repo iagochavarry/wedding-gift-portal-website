b79a61019c39973156eba72171b2e215e8386a4f
Iago Chavarry <iago@ng.cash>
2025-06-10 16:54:32 -0300

    fix gift button redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/features/Header/Header';
 import { Footer } from './components/features/Footer/Footer';
+import { ScrollToTop } from './components/ScrollToTop';
 import { Home } from './pages/Home/Home';
 import { Gifts } from './pages/Gifts/Gifts';
 import styles from './App.module.css';
@@ -8,6 +9,7 @@ import styles from './App.module.css';
 export const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className={styles.app}>
         <Header />
         <main className={styles.main}>
